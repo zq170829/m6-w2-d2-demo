c3f08dcd70f24291b59b007a298a937a7679d44f
Qi Zhang <zq170829@gmail.com>
2022-02-10 21:41:56 -0600

    submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Header from './features/header/Header'
+import TodoList from './features/todos/TodoList'
+import Footer from './features/footer/Footer'
+
+
 
 function App() {
   return (
@@ -6,15 +11,18 @@ function App() {
       <nav>
         <section>
           <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
         </section>
       </nav>
+      <main>
       <section>
-        <h2>Welcome to the Redux Fundamentals example app!</h2>
+        <h2>Todos</h2>
+        <div className="todoapp">
+          <Header />
+          <TodoList />
+          <Footer />
+        </div>
       </section>
+      </main>
     </div>
   )
 }
